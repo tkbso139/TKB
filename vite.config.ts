b13c6645f81359ced89119056a6d3f9f4d6f55a3
TKBSO139 <tkbso139@gmail.com>
2026-09-18 15:00:40 +0700

    build: update base path in vite config
    Adjust the base path to support dynamic deployment environments via GitHub Actions while maintaining local compatibility.

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import {defineConfig} from 'vite';
 
 export default defineConfig(() => {
   return {
-    base:'/lbgso/'.
+    base: process.env.GITHUB_ACTIONS ? '/TKB/' : './',
     plugins: [react(), tailwindcss()],
     resolve: {
       alias: {
